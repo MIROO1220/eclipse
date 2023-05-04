ad00f1717d667e28e8bc3936af2b73de6a64ee71
미루 <mir0_0@icloud.com>
2023-05-04 21:22:37 +0900

    09_polymorphism

---
+++ z09_polymorphism/src/edu/kh/poly/ex2/model/service/HMRCalculator.java
@@ -0,0 +1,49 @@
+package edu.kh.poly.ex2.model.service;
+
+import edu.kh.poly.ex2.model.vo.Animal;
+
+public class HMRCalculator extends Animal implements Calculator {
+						// 클래스, 인터페이스 동시 상속 가능
+						// 인터페이스는 다중 상속 가능
+	
+	// extends : 확장하다, implements : 구현한다
+	
+	// (부)클래스 - (자)클래스 상속 시에는 extends (추상 클래스도 포함)
+	
+	// (부)인터페이스 - (자)클래스 상속 시 implements
+	
+	
+	@Override
+	public int plus(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	@Override
+	public int minus(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	@Override
+	public int multiple(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	@Override
+	public double divide(int num1, int num2) {
+		return num1 / num2;
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void breath() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
