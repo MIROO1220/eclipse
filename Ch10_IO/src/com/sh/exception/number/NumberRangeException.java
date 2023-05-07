a259eeaffa4cc20a7befc5dbcf7cd8a932319dbf
미루 <130521373+MIROO1220@users.noreply.github.com>
2023-05-07 21:49:17 +0900

    10_IO

---
+++ Ch10_IO/src/com/sh/exception/number/NumberRangeException.java
@@ -0,0 +1,10 @@
+package com.sh.exception.number;
+
+public class NumberRangeException extends RuntimeException {
+	
+	public NumberRangeException() {}
+	
+	public NumberRangeException(String message) {
+		super(message);
+	}
+}
