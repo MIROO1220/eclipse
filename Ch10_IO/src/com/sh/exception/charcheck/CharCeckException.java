d5f3fa04a819ff7ae929b7a20027d7ceb98a4e7e
미루 <130521373+MIROO1220@users.noreply.github.com>
2023-05-07 21:47:37 +0900

    Add files via upload
    10_IO

---
+++ Ch10_IO/src/com/sh/exception/charcheck/CharCeckException.java
@@ -0,0 +1,11 @@
+package com.sh.exception.charcheck;
+
+public class CharCeckException extends RuntimeException {
+	
+	public CharCeckException() {}
+	
+	public CharCeckException(String message) {
+		super(message);
+	}
+
+}
---
+++ Ch10_IO/src/com/sh/exception/charcheck/CharCheckException.java
@@ -0,0 +1,11 @@
+package com.sh.exception.charcheck;
+
+public class CharCheckException extends RuntimeException {
+	
+	public CharCheckException() {}
+	
+	public CharCheckException(String message) {
+		super(message);
+	}
+
+}
